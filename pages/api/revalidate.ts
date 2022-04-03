f2540474731da3d42c09e4314485f0a032128c6b
Oooooiii <hearong.200@gmail.com>
2022-04-03 11:51:43 +0700

    fixed revalidate;

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,13 +1,21 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  try {
+    // Regenerate our index route showing the images
+    await res.unstable_revalidate('/')
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating')
+  }
 }
